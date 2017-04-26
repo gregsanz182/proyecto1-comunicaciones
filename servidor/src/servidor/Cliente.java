c9ea3ed58ae56118badba43fd54d5e219b267a9b
Anny Chacon <anny.chacon@unet.edu.ve>
2017-04-26 14:51:31 -0400

    version 1 sin interfaz (con errores)

--- servidor/src/servidor/Cliente.java
+++ servidor/src/servidor/Cliente.java
@@ -9,6 +9,7 @@ import java.io.*;
 import static java.lang.Thread.sleep;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.util.StringTokenizer;
 import java.util.logging.*;
 
 /**
@@ -20,9 +21,11 @@ public class Cliente {
     Socket socket;
     DataInputStream din;
     DataOutputStream dout;
-    BufferedOutputStream bufout;
-    BufferedInputStream bufin;
-    String nombre;
+    BufferedInputStream bin;
+    BufferedOutputStream bout;
+    String nombre, cadena, nomf;
+    int tamf;
+    StringTokenizer tokens;
     byte[] in, out;
     int bytes;
     File f;
@@ -30,30 +33,48 @@ public class Cliente {
     public Cliente() {
         socket = null;
         nombre = "Anny Chacon";
-        f = new File("C:/Users/Anny Chacon/Documents/NetBeansProjects/Servidor/imagen.jpg");
+        cadena = nomf = "";
+        tamf = 0;
+        tokens = null;
         try {
             socket = new Socket("localhost", 5000);
             din = new DataInputStream(socket.getInputStream());
             dout = new DataOutputStream(socket.getOutputStream());
-            bufout = new BufferedOutputStream(new FileOutputStream(f));
-            bufin = new BufferedInputStream(socket.getInputStream());
+            bin = new BufferedInputStream(socket.getInputStream());
         } catch (IOException ex) {
             Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
         }
         try {
             //client
-            System.out.println("Cliente: " + socket.getInetAddress() + " - " + nombre);
-            String ip = socket.getInetAddress() + " - " + nombre;
+            System.out.println("Cliente >> IP: " + socket.getInetAddress() + " Nombre: " + nombre);
+            cadena = socket.getInetAddress() + "?" + nombre + "\n";
             out = new byte[1024];
-            out = ip.getBytes(StandardCharsets.UTF_8);
+            out = cadena.getBytes(StandardCharsets.UTF_8);
             dout.write(out, 0, out.length);
             dout.flush();
             out = null;
             //server
-            in = new byte[512 * 1024];
-            bytes = bufin.read(in, 0, in.length);
+            in = new byte[1024];
+            bytes = din.read(in, 0, in.length);
             if (bytes > 0) {
-                bufout.write(in, 0, bytes);
+                cadena = new String(in, 0, bytes, StandardCharsets.UTF_8);
+                tokens = new StringTokenizer(cadena, "?\n");
+                nomf = tokens.nextToken();
+                tamf = Integer.parseInt(tokens.nextToken());
+            }
+            System.out.println("Servidor >> Nombre: " + nomf + " Tamaño: " + tamf);
+            System.out.println("Cadena: " + cadena);
+            f = new File("C:/Users/Anny Chacon/Desktop/proyecto1-comunicaciones/servidor/" + nomf);
+            bout = new BufferedOutputStream(new FileOutputStream(f));
+            //cliente
+
+            //server
+            in = new byte[tamf];
+            bytes = bin.read(in, 0, tamf);
+            System.out.println("bytes  -->>> " + bytes + " Tamaño -->>> " + tamf);
+            if (bytes > 0) {
+                bout.write(in, 0, bytes);
+                bout.flush();
             }
             //client
             String bye = "Adios";
@@ -61,15 +82,13 @@ public class Cliente {
             out = bye.getBytes(StandardCharsets.UTF_8);
             dout.write(out, 0, out.length);
             dout.flush();
-            sleep(5);
 
             dout.close();
             din.close();
+            bin.close();
             socket.close();
 
         } catch (IOException ex) {
-            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InterruptedException ex) {
             Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
--- servidor/src/servidor/Servidor.java
+++ servidor/src/servidor/Servidor.java
@@ -8,6 +8,7 @@ package servidor;
 import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.util.StringTokenizer;
 import java.util.logging.*;
 
 /**
@@ -20,14 +21,18 @@ public class Servidor {
     Socket socket;
     DataOutputStream dout;
     DataInputStream din;
-    BufferedOutputStream bufout;
-    BufferedInputStream bufin;
-    byte[] out, in;
+    BufferedOutputStream bout;
+    BufferedInputStream bin;
+    byte[] out, in, outf;
     File f;
+    String ip, nombre, cadena;
+    StringTokenizer tokens;
 
     public Servidor() {
         server = null;
         socket = null;
+        tokens = null;
+        cadena = "";
         f = new File("D:/Imagenes/fractals/1.jpg");
         try {
             server = new ServerSocket(5000);
@@ -41,39 +46,51 @@ public class Servidor {
             System.out.println("Cliente conectado...");
             dout = new DataOutputStream(socket.getOutputStream());
             din = new DataInputStream(socket.getInputStream());
-            bufin = new BufferedInputStream(new FileInputStream(f));
-            bufout = new BufferedOutputStream(socket.getOutputStream());
+            bout = new BufferedOutputStream(socket.getOutputStream());
+            bin = new BufferedInputStream(new FileInputStream(f));
         } catch (IOException ex) {
             Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
         }
         try {
-            //client
+            //client  ** IP - NOMBRE **
             in = new byte[1024];
             int bytes = din.read(in, 0, in.length);
             if (bytes > 0) {
-                System.out.println("Cliente: " + new String(in, 0, bytes, StandardCharsets.UTF_8));
+                cadena = new String(in, 0, bytes, StandardCharsets.UTF_8);
+                tokens = new StringTokenizer(cadena, "?\n");
+                ip = tokens.nextToken();
+                nombre = tokens.nextToken();
             }
+            System.out.println("Cliente >> IP: " + ip + " Nombre: " + nombre);
             in = null;
-            //server
-            out = new byte[524288]; // 512 kb
-            bytes = bufin.read(out, 0, out.length);
+
+            //server  ** NOMBRE ARCHIVO - TAMAÑO **
+            out = new byte[1024];
+            cadena = f.getName() + "?" + f.length() + "\n";
+            out = cadena.getBytes(StandardCharsets.UTF_8);
+            dout.write(out, 0, out.length);
+            System.out.println("Servidor >> " + cadena);
+
+            //server  ** IMAGEN **
+            out = new byte[(int) f.length()];
+            bytes = bin.read(out, 0, (int) f.length());
+            System.out.println("bytes >>>> " + bytes + " outf -->>>> " + out.length);
             if (bytes > 0) {
-                bufout.write(out, 0, bytes);
-                bufout.flush();
+                dout.write(out, 0, bytes);
+                dout.flush();
             }
             //client
-            String exit="";
+            String exit = "";
             in = new byte[1024];
             bytes = din.read(in, 0, in.length);
             if (bytes > 0) {
                 exit = new String(in, 0, bytes, StandardCharsets.UTF_8);
                 System.out.println("Cliente: " + exit);
             }
-            
+
             dout.close();
             din.close();
-            bufin.close();
-            bufout.close();
+            bout.close();
             socket.close();
 
         } catch (IOException ex) {

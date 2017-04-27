74591862a5c999a814dc147676bc649c592e6589
Greg Sanz <sanchz93@gmail.com>
2017-04-27 17:27:23 -0400

    Primer diseño de la interfaz. Sin hilos. Sin eventos

--- cliente/cliente/clienteGUI.Designer.cs
+++ cliente/cliente/clienteGUI.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace cliente
+﻿using System.Drawing;
+namespace cliente
 {
     partial class clienteGUI
     {
@@ -28,48 +29,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.labelServerIP = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelTextMaquina = new System.Windows.Forms.Label();
+            this.labelLocalIP = new System.Windows.Forms.Label();
+            this.labelMaquina = new System.Windows.Forms.Label();
+            this.labelTextLocalIP = new System.Windows.Forms.Label();
+            this.botonConectar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // labelServerIP
+            // 
+            this.labelServerIP.AutoSize = true;
+            this.labelServerIP.Location = new System.Drawing.Point(444, 10);
+            this.labelServerIP.Name = "labelServerIP";
+            this.labelServerIP.Size = new System.Drawing.Size(125, 13);
+            this.labelServerIP.TabIndex = 0;
+            this.labelServerIP.Text = "Dirección IP del servidor:";
+            this.labelServerIP.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(447, 26);
+            this.textBox1.MaxLength = 15;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelTextMaquina);
+            this.groupBox1.Controls.Add(this.labelLocalIP);
+            this.groupBox1.Controls.Add(this.labelMaquina);
+            this.groupBox1.Controls.Add(this.labelTextLocalIP);
+            this.groupBox1.Location = new System.Drawing.Point(29, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(377, 86);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Detalles del Cliente";
+            // 
+            // labelTextMaquina
+            // 
+            this.labelTextMaquina.AutoSize = true;
+            this.labelTextMaquina.Location = new System.Drawing.Point(142, 51);
+            this.labelTextMaquina.Name = "labelTextMaquina";
+            this.labelTextMaquina.Size = new System.Drawing.Size(55, 13);
+            this.labelTextMaquina.TabIndex = 3;
+            this.labelTextMaquina.Text = "NAME-PC";
+            // 
+            // labelLocalIP
+            // 
+            this.labelLocalIP.AutoSize = true;
+            this.labelLocalIP.Location = new System.Drawing.Point(26, 28);
+            this.labelLocalIP.Name = "labelLocalIP";
+            this.labelLocalIP.Size = new System.Drawing.Size(93, 13);
+            this.labelLocalIP.TabIndex = 0;
+            this.labelLocalIP.Text = "Direccion IP local:";
+            this.labelLocalIP.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelMaquina
+            // 
+            this.labelMaquina.AutoSize = true;
+            this.labelMaquina.Location = new System.Drawing.Point(26, 51);
+            this.labelMaquina.Name = "labelMaquina";
+            this.labelMaquina.Size = new System.Drawing.Size(116, 13);
+            this.labelMaquina.TabIndex = 2;
+            this.labelMaquina.Text = "Nombre de la maquina:";
+            // 
+            // labelTextLocalIP
+            // 
+            this.labelTextLocalIP.AutoSize = true;
+            this.labelTextLocalIP.Location = new System.Drawing.Point(122, 28);
+            this.labelTextLocalIP.Name = "labelTextLocalIP";
+            this.labelTextLocalIP.Size = new System.Drawing.Size(40, 13);
+            this.labelTextLocalIP.TabIndex = 1;
+            this.labelTextLocalIP.Text = "0.0.0.0";
+            this.labelTextLocalIP.Click += new System.EventHandler(this.labelTextLocalIP_Click);
+            // 
+            // botonConectar
+            // 
+            this.botonConectar.Enabled = false;
+            this.botonConectar.Location = new System.Drawing.Point(447, 52);
+            this.botonConectar.Name = "botonConectar";
+            this.botonConectar.Size = new System.Drawing.Size(204, 43);
+            this.botonConectar.TabIndex = 3;
+            this.botonConectar.Text = "Conectar";
+            this.botonConectar.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(81)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 384);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(684, 27);
+            this.panel1.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 119);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(8, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.Size = new System.Drawing.Size(71, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Dirección IP del servidor:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 135);
-            this.textBox1.MaxLength = 15;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(189, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Location = new System.Drawing.Point(12, 13);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(460, 80);
-            this.groupBox1.TabIndex = 2;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Detalles del Cliente";
+            this.label1.Text = "Preparando...";
+            this.label1.Click += new System.EventHandler(this.label1_Click_2);
             // 
             // clienteGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 411);
+            this.ClientSize = new System.Drawing.Size(684, 411);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.botonConectar);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelServerIP);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "clienteGUI";
             this.Text = "clienteGUI";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,8 +170,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelServerIP;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelLocalIP;
+        private System.Windows.Forms.Label labelTextLocalIP;
+        private System.Windows.Forms.Label labelMaquina;
+        private System.Windows.Forms.Label labelTextMaquina;
+        private System.Windows.Forms.Button botonConectar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- cliente/cliente/clienteGUI.cs
+++ cliente/cliente/clienteGUI.cs
@@ -4,7 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,12 +15,79 @@ namespace cliente
 {
     public partial class clienteGUI : Form
     {
+        String clienteIP;
+        String nombrePC;
+        private Color[] colores;
+
         public clienteGUI()
         {
             InitializeComponent();
+            iniThemeColors();
+            ejecutar();
+        }
+
+        public void ejecutar()
+        {
+            inicializarInfoCliente();
+        }
+
+        void iniThemeColors(){
+            colores = new Color[3];
+            colores[0] = Color.FromArgb(202, 81, 0);
+            colores[1] = Color.FromArgb(0, 122, 204);
+        }
+
+        private void inicializarInfoCliente()
+        {
+            clienteIP = hallarDireccionIP();
+            this.labelTextLocalIP.Text = clienteIP;
+            nombrePC = Environment.MachineName;
+            this.labelTextMaquina.Text = nombrePC;
+            this.textBox1.Enabled = true;
+            this.botonConectar.Enabled = true;
+            this.panel1.BackColor = colores[1];
+            this.label1.Text = "Listo";
+        }
+
+        String hallarDireccionIP()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return "0.0.0.0";
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelTextLocalIP_Click(object sender, EventArgs e)
         {
 
         }
